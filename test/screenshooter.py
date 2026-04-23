95543aa7b3364c4fa36f3becd315baba1cf4e1be
akira1442 <nawad.karihila@etu.sorbonne.universite.fr>
2026-04-23 17:49:48 +0200

    ajout png et debut screenshot pour le pathfinding

--- test/screenshooter.py
+++ test/screenshooter.py
@@ -5,16 +5,12 @@ import imutils
 import cv2
 import time
 
-keyboard = Controller()
-with keyboard.pressed(Key.cmd_r):
-    keyboard.press(Key.tab)
-    keyboard.release(Key.tab)
-
 time.sleep(2)
 
-image = pyautogui.screenshot()
-image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-cv2.imwrite("encounter_screen.png", image)
-image = imutils.resize(image, width=600)
-combee = image[97:178,180:274]
-cv2.imwrite("referencefemale.png", combee)
+
+#image = pyautogui.screenshot()
+#image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+#cv2.imwrite("encounter_screen.png", image)
+#image = imutils.resize(image, width=600)
+#combee = image[97:178,180:274]
+#cv2.imwrite("referencefemale.png", combee)
